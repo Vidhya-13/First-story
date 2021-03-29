d6ceb2b8b9217605cab49c7c5ec4e7fb2da13369
Vidhya Narayanasamy <78322128+Vidhya-13@users.noreply.github.com>
2021-03-29 13:44:57 +0200

    program to convert Celcius to Farenheit

---
+++ conversion to farenheit.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  6 22:28:04 2021
+
+@author: nvidh
+"""
+
+print ("Enter the degree: ")
+degree = int(input())
+print("The farenheit is ", +(degree * (9/5)) + 32)
