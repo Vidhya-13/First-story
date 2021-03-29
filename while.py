8d285b53fb6d78b99c32ab927c2feeeefc8d7793
Vidhya Narayanasamy <78322128+Vidhya-13@users.noreply.github.com>
2021-03-29 17:56:08 +0200

    Update while.py
    Deleted the irrelevant lines

--- while.py
+++ while.py
@@ -1,8 +1,7 @@
-#Spam = 0
-#while Spam < 5:
- #   print("Hello, World!")
-  #  Spam +=1
+Spam = 0
+while Spam < 5:
+    print("Hello, World!")
+    Spam +=1
 
 
-Age = 33
-print("I am", +Age , " years old")
+
